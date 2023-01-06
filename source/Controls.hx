8ae77c6f4fb6d48c338bf53bbf73ef03ec2b8e4c
MaysLastPlay <107422651+MaysLastPlayGithub@users.noreply.github.com>
2023-01-06 19:24:39 +0200

    Aoaoaoaooa

--- source/Controls.hx
+++ source/Controls.hx
@@ -759,6 +759,7 @@ class Controls extends FlxActionSet
 	 * Sets all actions that pertain to the binder to trigger when the supplied keys are used.
 	 * If binder is a literal you can inline this
 	 */
+        #if !android
 	public function bindKeys(control:Control, keys:Array<FlxKey>)
 	{
 		var copyKeys:Array<FlxKey> = keys.copy();
@@ -790,6 +791,7 @@ class Controls extends FlxActionSet
 		forEachBound(control, function(action, _) removeKeys(action, copyKeys));
 		#end
 	}
+   #end
 
 	inline static function addKeys(action:FlxActionDigital, keys:Array<FlxKey>, state:FlxInputState)
 	{
