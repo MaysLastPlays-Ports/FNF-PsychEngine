dbc3e94ee0c53cd001c203f922d3f7f824655fc3
MaysLastPlay <107422651+MaysLastPlayGithub@users.noreply.github.com>
2023-01-06 17:40:06 +0200

    Update MusicBeatSubstate.hx

--- source/MusicBeatSubstate.hx
+++ source/MusicBeatSubstate.hx
@@ -5,6 +5,7 @@ import flixel.FlxG;
 import flixel.FlxSubState;
 import flixel.FlxBasic;
 import flixel.FlxSprite;
+
 #if android
 import flixel.input.actions.FlxActionInput;
 import android.FlxVirtualPad;
