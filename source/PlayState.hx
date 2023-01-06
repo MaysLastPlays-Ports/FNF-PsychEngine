85393bd6012ac89265fc48edee767dff98261344
MaysLastPlay <107422651+MaysLastPlayGithub@users.noreply.github.com>
2023-01-06 14:36:58 +0200

    To make sure I don't forgot about it

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1177,6 +1177,10 @@ class PlayState extends MusicBeatState
 		timeTxt.cameras = [camHUD];
 		doof.cameras = [camHUD];
 
+                #if android
+                addAndroidControls();
+                #end
+
 		// if (SONG.song == 'South')
 		// FlxG.camera.alpha = 0.7;
 		// UI_camera.zoom = 1;
