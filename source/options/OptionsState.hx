b42257fa41ab0efc40082bcad670a0472b82e163
MaysLastPlay <107422651+MaysLastPlayGithub@users.noreply.github.com>
2023-01-07 10:57:29 +0200

    I'm pretty sure we don't need it on android

--- source/options/OptionsState.hx
+++ source/options/OptionsState.hx
@@ -29,7 +29,7 @@ using StringTools;
 
 class OptionsState extends MusicBeatState
 {
-	var options:Array<String> = ['Note Colors', 'Controls', 'Adjust Delay and Combo', 'Graphics', 'Visuals and UI', 'Gameplay'];
+	var options:Array<String> = ['Note Colors', #if desktop 'Controls', #end 'Adjust Delay and Combo', 'Graphics', 'Visuals and UI', 'Gameplay'];
 	private var grpOptions:FlxTypedGroup<Alphabet>;
 	private static var curSelected:Int = 0;
 	public static var menuBG:FlxSprite;
