53cacfe7e37414740b690d6e61a8f532b1b25ad9
MaysLastPlay <107422651+MaysLastPlayGithub@users.noreply.github.com>
2023-01-10 21:34:29 +0200

    Only sutil and editors...

--- source/ResetScoreSubState.hx
+++ source/ResetScoreSubState.hx
@@ -2,6 +2,7 @@ import flixel.FlxG;
 import flixel.FlxSprite;
 import flixel.FlxSubState;
 import flixel.util.FlxColor;
+import flixel.addons.transition.FlxTransitionableState;
 
 using StringTools;
 
@@ -69,6 +70,12 @@ class ResetScoreSubState extends MusicBeatSubstate
 		noText.x += 200;
 		add(noText);
 		updateOptions();
+
+               #if android
+                addVirtualPad(LEFT_RIGHT, A_B);
+                addPadCamera();
+                #end
+
 	}
 
 	override function update(elapsed:Float)
@@ -89,7 +96,12 @@ class ResetScoreSubState extends MusicBeatSubstate
 		}
 		if(controls.BACK) {
 			FlxG.sound.play(Paths.sound('cancelMenu'), 1);
-			close();
+			#if android
+                        FlxTransitionableState.skipNextTransOut = true;
+			FlxG.resetState();
+                        #else
+                        close();
+                        #end
 		} else if(controls.ACCEPT) {
 			if(onYes) {
 				if(week == -1) {
@@ -99,7 +111,12 @@ class ResetScoreSubState extends MusicBeatSubstate
 				}
 			}
 			FlxG.sound.play(Paths.sound('cancelMenu'), 1);
-			close();
+			#if android
+                        FlxTransitionableState.skipNextTransOut = true;
+			FlxG.resetState();
+                        #else
+                        close();
+                        #end
 		}
 		super.update(elapsed);
 	}
