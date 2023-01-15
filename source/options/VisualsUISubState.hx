870d0a1ba20cddf9fa844d81d362d62637830f12
MaysLastPlay <107422651+MaysLastPlayGithub@users.noreply.github.com>
2023-01-15 13:10:50 +0200

    Fps Counter Fix

--- source/options/VisualsUISubState.hx
+++ source/options/VisualsUISubState.hx
@@ -89,7 +89,6 @@ class VisualsUISubState extends BaseOptionsMenu
 		option.decimals = 1;
 		addOption(option);
 		
-		#if !android
 		var option:Option = new Option('FPS Counter',
 			'If unchecked, hides FPS Counter.',
 			'showFPS',
@@ -97,7 +96,6 @@ class VisualsUISubState extends BaseOptionsMenu
 			#if android false #else true #end);
 		addOption(option);
 		option.onChange = onChangeFPSCounter;
-		#end
 		
 		var option:Option = new Option('Pause Screen Song:',
 			"What song do you prefer for the Pause Screen?",
