a460a60ffdea7498e469559c08be410de8639b7d
MaysLastPlay <107422651+MaysLastPlayGithub@users.noreply.github.com>
2023-01-21 21:44:33 +0200

    Oops

--- source/ModsMenuState.hx
+++ source/ModsMenuState.hx
@@ -378,7 +378,7 @@ class ModsMenuState extends MusicBeatState
 		FlxG.mouse.visible = true;
 
                 #if android
-                addVirtualPad(UP_DOWN, A_B);
+                addVirtualPad(UP_DOWN, B);
                 #end
 
 		super.create();
