b9ff4801a5b989ed82e78eccb19698a9914b8e93
Nuno-Filipe-Studios <109983796+Nuno-Filipe-Studios@users.noreply.github.com>
2023-01-28 18:06:26 +0100

    Update FlxRuntimeShader.hx

--- source/flixel/addons/display/FlxRuntimeShader.hx
+++ source/flixel/addons/display/FlxRuntimeShader.hx
@@ -653,22 +653,6 @@ class FlxRuntimeShader extends FlxShader
 	}
 
 	/**
-	 * Set or modify a sampler2D input of the shader.
-	 * @param name The name of the shader input to modify.
-	 * @param value The texture to use as the sampler2D input.
-	 */
-	public function setSampler2D(name:String, value:BitmapData)
-	{
-		var prop:ShaderInput<BitmapData> = Reflect.field(this.data, name);
-		if(prop == null)
-		{
-			trace('[WARNING] Shader sampler2D property ${name} not found.');
-			return;
-		}
-		prop.input = value;
-	}
-
-	/**
 	 * Modify a bitmap data parameter of the shader.
 	 * @param name The name of the parameter to modify.
 	 * @param value The new value to use.
