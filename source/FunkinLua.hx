83035a497d57916e87f7321e46a18b2488c37489
MaysLastPlay <107422651+MaysLastPlayGithub@users.noreply.github.com>
2023-02-01 19:05:55 +0200

    update FunkinLua.hx

--- source/FunkinLua.hx
+++ source/FunkinLua.hx
@@ -2897,16 +2897,16 @@ class FunkinLua {
 				var frag:String = folder + name + '.frag';
 				var vert:String = folder + name + '.vert';
 				var found:Bool = false;
-				if(FileSystem.exists(SUtil.getPath() + frag))
+				if(FileSystem.exists(frag))
 				{
-					frag = File.getContent(SUtil.getPath() + frag);
+					frag = File.getContent(frag);
 					found = true;
 				}
 				else frag = null;
 
-				if(FileSystem.exists(SUtil.getPath() + vert))
+				if(FileSystem.exists(vert))
 				{
-					vert = File.getContent(SUtil.getPath() + vert);
+					vert = File.getContent(vert);
 					found = true;
 				}
 				else vert = null;
