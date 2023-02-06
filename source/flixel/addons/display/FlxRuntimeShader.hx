dda8181c7b1ea508654700d834cbbc8e76231e2f
MaysLastPlay <107422651+MaysLastPlayGithub@users.noreply.github.com>
2023-02-06 16:46:00 +0200

    Aaaaa

--- source/flixel/addons/display/FlxRuntimeShader.hx
+++ source/flixel/addons/display/FlxRuntimeShader.hx
@@ -111,27 +111,27 @@ class FlxRuntimeShader extends FlxShader
 	;
 	#end
 	static final BASE_FRAGMENT_BODY:String = "
-		vec4 color = texture2D (bitmap, openfl_TextureCoordv);
+		vec4 color = texture2D(bitmap, openfl_TextureCoordv);
 		if (color.a == 0.0)
 		{
-			gl_FragColor = vec4 (0.0, 0.0, 0.0, 0.0);
+			gl_FragColor = vec4(0.0, 0.0, 0.0, 0.0);
 		}
 		else if (openfl_HasColorTransform)
 		{
-			color = vec4 (color.rgb / color.a, color.a);
+			color = vec4(color.rgb / color.a, color.a);
 			mat4 colorMultiplier = mat4 (0);
 			colorMultiplier[0][0] = openfl_ColorMultiplierv.x;
 			colorMultiplier[1][1] = openfl_ColorMultiplierv.y;
 			colorMultiplier[2][2] = openfl_ColorMultiplierv.z;
 			colorMultiplier[3][3] = 1.0; // openfl_ColorMultiplierv.w;
-			color = clamp (openfl_ColorOffsetv + (color * colorMultiplier), 0.0, 1.0);
+			color = clamp(openfl_ColorOffsetv + (color * colorMultiplier), 0.0, 1.0);
 			if (color.a > 0.0)
 			{
-				gl_FragColor = vec4 (color.rgb * color.a * openfl_Alphav, color.a * openfl_Alphav);
+				gl_FragColor = vec4(color.rgb * color.a * openfl_Alphav, color.a * openfl_Alphav);
 			}
 			else
 			{
-				gl_FragColor = vec4 (0.0, 0.0, 0.0, 0.0);
+				gl_FragColor = vec4(0.0, 0.0, 0.0, 0.0);
 			}
 		}
 		else
