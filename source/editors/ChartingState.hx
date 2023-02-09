25e974ab1693fc5ec40f75d1bc5d2687225d5d07
MaysLastPlay <107422651+MaysLastPlayGithub@users.noreply.github.com>
2023-02-09 19:51:55 +0200

    Hmmmmm

--- source/editors/ChartingState.hx
+++ source/editors/ChartingState.hx
@@ -392,7 +392,7 @@ class ChartingState extends MusicBeatState
 		updateGrid();
 
 		#if android
-		addVirtualPad(CHART_EDITOR, A_B_C_X_Y_Z);
+		addVirtualPad(FULL, A_B_C_X_Y_Z);
 		#end
 
 		super.create();
