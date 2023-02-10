656318f1654eddfc39505e84c5887984a2eb03d9
MaysLastPlay <107422651+MaysLastPlayGithub@users.noreply.github.com>
2023-02-10 14:16:56 +0200

    update PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1413,7 +1413,7 @@ class PlayState extends MusicBeatState
 		#end
 	}
 
-	public function initLuaShader(name:String, ?glslVersion:Int = 120)
+	public function initLuaShader(name:String, ?glslVersion:Int = 120es)
 	{
 		if(!ClientPrefs.shaders) return false;
 
