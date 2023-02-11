dcebb24a7a8192452b7e4fa9fa0a3180ce6e2e37
MaysLastPlay <107422651+MaysLastPlayGithub@users.noreply.github.com>
2023-02-11 08:09:11 +0200

    Let's see again

--- source/flixel/addons/display/FlxRuntimeShader.hx
+++ source/flixel/addons/display/FlxRuntimeShader.hx
@@ -196,7 +196,7 @@ class FlxRuntimeShader extends FlxShader
 	 * @param vertexSource The vertex shader source.
 	 * Note you also need to `initialize()` the shader MANUALLY! It can't be done automatically.
 	 */
-	public function new(fragmentSource:String = null, vertexSource:String = null, glslesVersion:Int = 100):Void
+	public function new(fragmentSource:String = null, vertexSource:String = null):Void
 	{
 		_glslesVersion = glslesVersion;
 
