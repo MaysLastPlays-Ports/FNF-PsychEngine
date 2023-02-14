32ed5364fce9cec6a3e6aa43f70e19e2b2f6cb3e
MaysLastPlay <107422651+MaysLastPlayGithub@users.noreply.github.com>
2023-02-14 19:00:47 +0200

    Update FlxNewHitbox.hx

--- source/android/FlxNewHitbox.hx
+++ source/android/FlxNewHitbox.hx
@@ -4,7 +4,7 @@ import flixel.FlxG;
 import flixel.group.FlxSpriteGroup;
 import openfl.display.BitmapData;
 import openfl.display.Shape;
-import mobile.flixel.FlxButton;
+import android.flixel.FlxButton;
 
 /**
  * A zone with 4 hint's (A hitbox).
