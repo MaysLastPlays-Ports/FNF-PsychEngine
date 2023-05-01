db3bad854b339fe77d958b43249d62821de8a370
Stefan Menićanin <126715034+Stefan2008Git@users.noreply.github.com>
2023-05-01 17:51:06 +0200

    Some Android additions from old port

--- source/ClientPrefs.hx
+++ source/ClientPrefs.hx
@@ -10,7 +10,7 @@ class ClientPrefs {
 	public static var downScroll:Bool = false;
 	public static var middleScroll:Bool = false;
 	public static var opponentStrums:Bool = true;
-	public static var showFPS:Bool = true;
+	public static var showFPS:Bool = #if android false #else true #end;
 	public static var flashing:Bool = true;
 	public static var globalAntialiasing:Bool = true;
 	public static var noteSplashes:Bool = true;
--- source/FlashingState.hx
+++ source/FlashingState.hx
@@ -23,6 +23,17 @@ class FlashingState extends MusicBeatState
 		var bg:FlxSprite = new FlxSprite().makeGraphic(FlxG.width, FlxG.height, FlxColor.BLACK);
 		add(bg);
 
+		#if android
+		warnText = new FlxText(0, 0, FlxG.width,
+			"Hey, watch out!\n
+			Be careful when you touch the phone fast!\n
+			You can break your phone screen if you do that,also\n
+			This Mod contains some flashing lights!\n
+			Press A to disable them now or go to Options Menu.\n
+			Press B to ignore this message.\n
+			You've been warned!",
+			32);
+		#else
 		warnText = new FlxText(0, 0, FlxG.width,
 			"Hey, watch out!\n
 			This Mod contains some flashing lights!\n
@@ -30,6 +41,7 @@ class FlashingState extends MusicBeatState
 			Press ESCAPE to ignore this message.\n
 			You've been warned!",
 			32);
+		#end
 		warnText.setFormat("VCR OSD Mono", 32, FlxColor.WHITE, CENTER);
 		warnText.screenCenter(Y);
 		add(warnText);
