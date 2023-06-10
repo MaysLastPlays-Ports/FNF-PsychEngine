8de7f121ef6776678007e502617224cf391adf9d
Hiho2950 <109638489+Hiho2950@users.noreply.github.com>
2023-06-10 19:05:02 +0100

    Update FunkinLua.hx

--- source/FunkinLua.hx
+++ source/FunkinLua.hx
@@ -916,17 +916,17 @@ class FunkinLua {
 		Lua_helper.add_callback(lua, "addHxScript", function(tag:String, fileToAdd:String) {
 			if (PlayState.instance.HxCodes.exists(tag))
 			{
-				PlayState.instance.HxCodes.get(tag) = new SScriptHandler(SUtil.getPath() + fileToRun);
+				PlayState.instance.HxCodes.get(tag):SScriptHandler = new SScriptHandler(SUtil.getPath() + fileToRun);
 			}
 		});
 		
 		Lua_helper.add_callback(lua, "callHx", function(tag:String, func:String, ?retTF:Bool = false, ?classToRun:String = null) {
 			if (PlayState.instance.HxCodes.exists(tag))
 			{
-				if (!classToRun == null) {
-					var funcRun = PlayState.instance.HxCodes.get(tag).call(func, classToRun);
-				} else {
+				if (classToRun == null) {
 					var funcRun = PlayState.instance.HxCodes.get(tag).call(func);
+				} else {
+					var funcRun = PlayState.instance.HxCodes.get(tag).call(func, classToRun);
 				}
 				if (retTf) return funcRun.returnValue;
 			}
